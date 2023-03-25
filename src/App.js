caab10cdf2a581b7a64a2767095e08548160c453
marcsouth <104800775+marcsouth@users.noreply.github.com>
2023-03-24 23:41:36 -0400

    landing and about done

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
 
   <Navigation />
 
-<div className="landingpage w-screen h-screen">
+<div className="landingpage w-screen h-screen bg-gradient-to-b from-transparent to-black">
 
   <div className="flex justify-center items-center flex-col text-center p-4">
 
-    <h1 className='text-lime-500 text-7xl mt-40 mb-10 font-extrabold  p-4 flex'>MIKE CREPON FITNESS</h1>
+    <h1 className='text-lime-500 text-7xl mt-40 mb-10 font-extrabold  p-4 flex drop-shadow-md'>MIKE CREPON FITNESS</h1>
 
-  
+
     <Link to="contact" spy={true} smooth={true} offset={50} duration={500} className='text-l text-lime-00  text-slate-200  bg-black   border border-slate-200 p-4 mb-10 rounded-lg '  > Book a Consultation today</Link>
  
 
